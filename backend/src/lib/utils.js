04596cea41f24a9b63f6b616c9ee2580106a356e
Suman Kayal <suman13kayal@gmail.com>
2025-12-02 12:52:59 +0530

    backend socket server update

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -23,10 +23,11 @@ export const generateToken = (userId, res) => {
 
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-        maxAge: 24 * 60 * 60 * 1000, // 1 day
+        secure: true,
+        sameSite: "none",
+        maxAge: 24 * 60 * 60 * 1000,
     });
+
 
     return token;
 };
